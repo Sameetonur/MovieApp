923ca020913de325e19fc6239655a219ef86d477
Emre Kılıç <emre123126@gmail.com>
2024-11-07 22:00:34 +0300

    Partial işlemi yapıldı
    Partial işlemini yapıldı

--- MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3098d6a4347a248e0dbca60056e2a0c0ab041f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e072478da7552fbbcc87a61c5efd442b84a605")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
