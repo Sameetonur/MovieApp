ea37c9ca0ff52fdb0b16e6fe4ba5a25f13567aa1
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-11-09 21:14:44 +0300

    asdasd
    sadas

--- MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ae1478c45215e32ec00d8fb62a05c2cba80b90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7081f73f6dc1627b3bb62058c8620f1fc307a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
