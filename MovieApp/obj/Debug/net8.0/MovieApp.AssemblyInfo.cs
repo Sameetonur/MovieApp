fbbfaafaf7cca292ddbc56002c6f2e569fc68782
bedirhancyl <bedirhanbiraycayli@gmail.com>
2024-11-13 21:10:33 +0300

    contact controller oluşturuldu

--- MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fac2054fa0ae50d501004b2c2b10b9a61a3cb56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f740dfb2778b0ef2b38d06527593e9fd56768d09")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
