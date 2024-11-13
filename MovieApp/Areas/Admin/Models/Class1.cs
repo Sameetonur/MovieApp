1b785ded255ab3d8557c7789cfbd713735fb94e3
tahirburak2 <63349484+tahirburak2@users.noreply.github.com>
2024-11-13 16:58:42 +0300

    Admin paneli oluşturuldu template giydirildi

---
+++ MovieApp/Areas/Admin/Models/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MovieApp.Areas.Admin.Models;
+
+public class Class1
+{
+// bu modeli viewImportsta modeli de kullanabilmesi için oluşturdum silip yerine başka model oluşturabilirsiniz
+}
