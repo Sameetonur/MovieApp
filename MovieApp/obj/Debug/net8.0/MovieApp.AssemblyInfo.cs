6a5dcd4b5757ddd4b0b9fcdd8951fc503c0cdbeb
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-11-14 19:41:17 +0300

    Ana Sayfa Tipi ve Veri Tabano Güncellendi
    ansayfa tipi ve veri tabanı güncellendi veri tabanına gerekli linkler yerleştirilid wwwroota img lerin içinde filmler klasörü eklenip resimler oraya yerleştirildi

--- MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8112336aec65b84a6680b8b7b037ff2c1e3b3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9669d9f9f1b1768803956be4ad5ecfe657f7de6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
